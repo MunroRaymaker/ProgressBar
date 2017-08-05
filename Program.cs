32f6633df936b18eefb1bbc176462b503c5e238a
MunroRaymaker <jenrud@gmail.com>
2017-08-05 09:30:51 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Progress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            Console.WriteLine("Performing time consuming task...");
+
+            using(var progress = new ProgressBar())
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    progress.Report((double) i / 100);
+                    System.Threading.Thread.Sleep(200);
+                }
+            }
+
+            Console.WriteLine("Task is done!");
+
+            Console.WriteLine("Fibonacci numbers 1-15:");
+
+            for(int i = 0; i < 15; i++)            
+            {
+                Console.WriteLine($"{i+1}: {FibonacciNumber(i)}");
+            }
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for(int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b; 
+                b += tmp; 
+            }
+
+            return a;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Console Progress Bar
+
+Simple inline progress bar for the console, build using dotnet core 1.1 CLI. 
+
+Usage:
+
+```
+dotnet run -- John
+```
